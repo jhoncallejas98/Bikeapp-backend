f2e66aa101be523b8d7eadb831db63cb031de16a
Jhon <jhoncallejas1998@hotmail.com>
2025-07-21 20:21:16 -0500

    ruta cambiada

--- src/routes/bike.route.mjs
+++ src/routes/bike.route.mjs
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import  {createBike, getAllBikes, getBikeById, updateBikeById, removeBikeById} from '../controller/bikes.controllers.mjs';
+import  {createBike, getAllBikes, getTodayBikeById, updateBikeById, removeTodayBikeById} from '../controller/bikes.controllers.mjs';
 
 const router = Router();
 
 router.post('/api/bike', createBike);
 router.get('/api/bike', getAllBikes);
-router.get('/api/bike/:id', getBikeById);
+router.get('/api/bike/:id', getTodayBikeById);
 router.put('/api/bike/:id', updateBikeById);
-router.delete('/api/bike/:id', removeBikeById);
+router.delete('/api/bike/:id', removeTodayBikeById);
 
 export default router;
